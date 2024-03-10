23ec7e8e555fb39007949574ed9d41404ba85226
shresht8 <shresht.shetty@gmail.com>
2024-03-10 19:02:19 +1000

    shresht update new cv template, user path

--- bot_create_cv.py
+++ bot_create_cv.py
@@ -13,23 +13,35 @@ from langchain.chains.openai_functions import (
 )
 import argparse
 
+
 class JobDescription(BaseModel):
     """Extracting detailed information about a job description parsed from HTML text
     containing some relevant and some irrelevant text"""
+
     job_title: str = Field(..., description="The job title/position offered by company")
     job_overview: str = Field(..., description="Overview of the job")
     company_name: str = Field(..., description="Company name offering the job")
     about_company: str = Field(..., description="About the company")
-    job_responsibilities: str = Field(..., description="responsibilities of the role. Use bullet points if possible")
-    job_requirements: str = Field(..., description="requirements/experience/ to get the job. Use bullet points if you "
-                                                   "can")
-    skills_required: Optional[str] = Field(None, description="skills required for the job. Use bullet points if you can")
+    job_responsibilities: str = Field(
+        ..., description="responsibilities of the role. Use bullet points if possible"
+    )
+    job_requirements: str = Field(
+        ...,
+        description="requirements/experience/ to get the job. Use bullet points if you "
+        "can",
+    )
+    skills_required: Optional[str] = Field(
+        None, description="skills required for the job. Use bullet points if you can"
+    )
     salary_range: Optional[int] = Field(None, description="salary range for the job")
-    company_perks: Optional[str] = Field(None, description="Benefits offered by the company for the role. Use bullet "
-                                                           "points if you can")
+    company_perks: Optional[str] = Field(
+        None,
+        description="Benefits offered by the company for the role. Use bullet "
+        "points if you can",
+    )
 
 
-class BotCreateCV():
+class BotCreateCV:
     def __init__(self, user_name, user_info_path, job_desc_link, cv_template_path):
         self.job_desc_str = None
         self.latex_content = None
@@ -41,26 +53,36 @@ class BotCreateCV():
         self.preprocess_user_date()
         self.scrape_job_desc()
         self.read_cv_template()
+        self.read_job_desc()
 
     def preprocess_user_date(self):
         """reads and preprocesses user info, add logo metadata to user info doc"""
-        with open(self.user_info_path, 'r', encoding='utf-8') as file:
+        with open(
+            os.path.join(self.user_info_path, "User_Professional_Information.txt"),
+            "r",
+            encoding="utf-8",
+        ) as file:
             self.user_info_str = file.read()
 
     def read_cv_template(self):
         """reads cv main.tex file from the directory"""
-        with open(os.path.join(self.cv_template_path, 'main.tex'), 'rb') as file:
+        with open(os.path.join(self.cv_template_path, "main.tex"), "rb") as file:
             latex_content_bytes = file.read()
-        self.latex_content = latex_content_bytes.decode('utf-8')
+        self.latex_content = latex_content_bytes.decode("utf-8")
 
     def read_job_desc(self):
         """reads the job description from the path"""
-        with open(os.path.join(self.cv_template_path, 'job_description.txt'), 'w') as file:
+        with open(
+            os.path.join(self.user_info_path, f"{self.user_name}_job_description.txt"),
+            "r",
+        ) as file:
             self.job_desc_str = file.read()
 
     def scrape_job_desc(self):
-        """Scrapes job data from link and """
-        if not os.path.exists(os.path.join(self.cv_template_path, 'job_description.txt')):
+        """Scrapes job data from link and"""
+        if not os.path.exists(
+            os.path.join(self.user_info_path, f"{self.user_name}_job_description.txt")
+        ):
             url = [self.job_desc_link]
             loader = AsyncHtmlLoader(url)
             docs = loader.load()
@@ -74,19 +96,30 @@ class BotCreateCV():
         llm = ChatOpenAI(model="gpt-4", temperature=0)
         prompt = ChatPromptTemplate.from_messages(
             [
-                ("system", "You are a world class algorithm for extracting information in structured formats."),
-                ("human", "Use the given format to extract detailed information from the following input: {input}"),
+                (
+                    "system",
+                    "You are a world class algorithm for extracting information in structured formats.",
+                ),
+                (
+                    "human",
+                    "Use the given format to extract detailed information from the following input: {input}",
+                ),
                 ("human", "Tip: Make sure to answer in the correct format"),
             ]
         )
 
-        chain = create_structured_output_chain(JobDescription, llm, prompt, verbose=True)
+        chain = create_structured_output_chain(
+            JobDescription, llm, prompt, verbose=True
+        )
         job_description = chain.run(html_text[0].page_content)
         return job_description
 
     def write_jd_to_txt(self, job_desc: JobDescription):
         """Write job description object to text file"""
-        with open(os.path.join(self.cv_template_path, 'job_description.txt'), 'w') as file:
+        with open(
+            os.path.join(self.user_info_path, f"{self.user_name}_job_description.txt"),
+            "w",
+        ) as file:
             file.write("Job Title: {}\n".format(job_desc.job_title))
             file.write("\n")
             file.write("Company Overview:\n{}".format(job_desc.about_company))
@@ -95,7 +128,9 @@ class BotCreateCV():
             file.write("\n")
             file.write("About the job:\n{}\n".format(job_desc.job_overview))
             file.write("\n")
-            file.write("Job responsibilities:\n{}\n".format(job_desc.job_responsibilities))
+            file.write(
+                "Job responsibilities:\n{}\n".format(job_desc.job_responsibilities)
+            )
             file.write("\n")
             file.write("Job requirements:\n{}\n".format(job_desc.job_requirements))
             file.write("\n")
@@ -111,26 +146,23 @@ class BotCreateCV():
 
     def generate_cv(self):
         """initializes llm, creates cv tex file and compiles it"""
-        CV_EXPERT_BOT = CVExpertBot(self.user_name,
-                                    self.user_info_str,
-                                    self.job_desc_str,
-                                    self.latex_content)
-        CV_EXPERT_BOT.generate_latex_output(self.cv_template_path)
+        CV_EXPERT_BOT = CVExpertBot(
+            self.user_name, self.user_info_str, self.job_desc_str, self.latex_content
+        )
+        prompt_str_input = CV_EXPERT_BOT.generate_latex_output(self.cv_template_path)
+        return prompt_str_input
         # CV_EXPERT_BOT.compile_tex_file(self.cv_template_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="generates CV latex code")
     parser.add_argument("arg1", help="Name of user")
     parser.add_argument("arg2", help="Path to user info directory")
     parser.add_argument("arg3", help="Job URL")
     parser.add_argument("arg4", help="CV Template path")
     args = parser.parse_args()
-    os.environ['OPENAI_API_KEY'] = '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'
-    bot_create_cv = BotCreateCV(args.arg1,
-                                args.arg2,
-                                args.arg3,
-                                args.arg4)
+    os.environ["OPENAI_API_KEY"] = "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"
+    bot_create_cv = BotCreateCV(args.arg1, args.arg2, args.arg3, args.arg4)
     print("args passed")
-    bot_create_cv.generate_cv()
+    prompt_str = bot_create_cv.generate_cv()
     print("run completed")
--- cv_expert_bot.py
+++ cv_expert_bot.py
@@ -10,9 +10,8 @@ from transformers import GPT2Tokenizer
 # Initialize the tokenizer
 
 
-
-class CVExpertBot():
-    def __init__(self, user_name ,user_info_str, job_desc_str, latex_code_str):
+class CVExpertBot:
+    def __init__(self, user_name, user_info_str, job_desc_str, latex_code_str):
         self.user_name = user_name
         self.user_info_str = user_info_str
         self.job_desc_str = job_desc_str
@@ -22,44 +21,65 @@ class CVExpertBot():
         self.count_input_tokens()
 
     def count_input_tokens(self):
-        formatted_prompt = LATEX_PROMPT.format(LATEX_CODE=self.latex_code_str, USER_INFORMATION=self.user_info_str,
-                                         JOB_INFORMATION=self.job_desc_str)
+        formatted_prompt = LATEX_PROMPT.format(
+            LATEX_CODE=self.latex_code_str,
+            USER_INFORMATION=self.user_info_str,
+            JOB_INFORMATION=self.job_desc_str,
+        )
         # Tokenize and count
         tokenized_prompt = self.tokenizer.encode(formatted_prompt)
         number_of_tokens = len(tokenized_prompt)
 
         print(f"Number of tokens in the prompt: {number_of_tokens}")
 
-
     def __create_llm_chain(self):
         """initializes the llm chain"""
-        llm = ChatOpenAI(model_name='gpt-4', temperature=0)
+        llm = ChatOpenAI(model_name="gpt-4", temperature=0)
         self.llm_chain = LLMChain(
             llm=llm,
-            prompt=PromptTemplate(input_variables=['LATEX_CODE', 'USER_INFORMATION', 'JOB_INFORMATION'],
-                                  template=LATEX_PROMPT),
-            output_key='input_latex_code'
+            prompt=PromptTemplate(
+                input_variables=["LATEX_CODE", "USER_INFORMATION", "JOB_INFORMATION"],
+                template=LATEX_PROMPT,
+            ),
+            output_key="input_latex_code",
         )
         self.correction_chain = LLMChain(
             llm=llm,
-            prompt=PromptTemplate(input_variables=['input_latex_code'], template=CHECK_SYNTAX_PROMPT),
-            output_key="output_latex_code"
+            prompt=PromptTemplate(
+                input_variables=["input_latex_code"], template=CHECK_SYNTAX_PROMPT
+            ),
+            output_key="output_latex_code",
         )
-        self.overall_chain = SequentialChain(chains=[self.llm_chain, self.correction_chain],
-                                             input_variables=['LATEX_CODE', 'USER_INFORMATION', 'JOB_INFORMATION'],
-                                             output_variables=['output_latex_code'],
-                                             verbose=True)
+        self.overall_chain = SequentialChain(
+            chains=[self.llm_chain, self.correction_chain],
+            input_variables=["LATEX_CODE", "USER_INFORMATION", "JOB_INFORMATION"],
+            output_variables=["output_latex_code"],
+            verbose=True,
+        )
 
     def generate_latex_output(self, path):
         """generates customised latex output using the llm chain"""
         # output_str = self.overall_chain({"LATEX_CODE":self.latex_code_str, "USER_INFORMATION": self.user_info_str,
         #                                  "JOB_INFORMATION":self.job_desc_str})
-        output_str = self.llm_chain.predict(LATEX_CODE=self.latex_code_str, USER_INFORMATION=self.user_info_str,
-                                         JOB_INFORMATION=self.job_desc_str)
+        output_str = self.llm_chain.predict(
+            LATEX_CODE=self.latex_code_str,
+            USER_INFORMATION=self.user_info_str,
+            JOB_INFORMATION=self.job_desc_str,
+        )
+        input_str = LATEX_PROMPT.format(
+            LATEX_CODE=self.latex_code_str,
+            USER_INFORMATION=self.user_info_str,
+            JOB_INFORMATION=self.job_desc_str,
+        )
         self.count_output_tokens(output_str)
-        with open(os.path.join(path, '{name}_CV.tex'.format(name=self.user_name)), 'w', encoding='utf-8') as tex_file:
-        # with open(os.path.join(path, 'main.tex'.format(name=self.user_name)), 'w', encoding='utf-8') as tex_file:
+        with open(
+            os.path.join(path, "{name}_CV.tex".format(name=self.user_name)),
+            "w",
+            encoding="utf-8",
+        ) as tex_file:
+            # with open(os.path.join(path, 'main.tex'.format(name=self.user_name)), 'w', encoding='utf-8') as tex_file:
             tex_file.write(output_str)
+        return input_str
 
     def count_output_tokens(self, output_string):
         # Tokenize and count
@@ -67,5 +87,3 @@ class CVExpertBot():
         number_of_tokens = len(tokenized_prompt)
 
         print(f"Number of tokens in the output: {number_of_tokens}")
-
-
